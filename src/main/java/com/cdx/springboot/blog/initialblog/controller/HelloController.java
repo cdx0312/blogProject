982a17d101166fb0da4159cb7a9e470b719edb4c
cdx <cdxu0312@outlook.com>
2018-11-04 20:47:58 +0800

    test of this projects

---
+++ src/main/java/com/cdx/springboot/blog/initialblog/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.cdx.springboot.blog.initialblog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: initialblog
+ * @author: cdx
+ * @create: 2018-11-04 20:28
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
