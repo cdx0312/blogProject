a5edb8bd4b7e5e9ec2ea20ccc264d368790bad39
cdx <cdxu0312@outlook.com>
2018-11-06 19:13:40 +0800

    add themeleaf module

---
+++ src/main/java/com/cdx/springboot/blog/initialblog/controller/UserController.java
@@ -0,0 +1,96 @@
+package com.cdx.springboot.blog.initialblog.controller;
+
+import com.cdx.springboot.blog.initialblog.domain.User;
+import com.cdx.springboot.blog.initialblog.repository.UserRepository;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * @program: initialblog
+ * @author: cdx
+ * @create: 2018-11-04 21:25
+ **/
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     *  获取用户列表
+     * @return
+     */
+    private List<User> getUserList() {
+        return userRepository.listUser();
+    }
+
+    @GetMapping
+    public ModelAndView list(Model model) {
+        model.addAttribute("userList", getUserList());
+        model.addAttribute("title", "用户管理");
+        return new ModelAndView("users/list", "userModel", model);
+    }
+
+    /**
+     * 根据ID查询用户
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("{id}")
+    public ModelAndView view(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("title", "查看用户");
+        return new ModelAndView("users/view", "userModel", model);
+    }
+
+    /**
+     * 获取form表单页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/form")
+    public ModelAndView createForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "创建用户");
+        return new ModelAndView("users/form", "userModel", model);
+    }
+
+    /**
+     * 新建用户
+     * @param user
+     * @return
+     */
+    @PostMapping
+    public ModelAndView create(User user) {
+        user = userRepository.saveOrUpdateUser(user);
+        return new ModelAndView("redirect:/users");
+    }
+
+    @GetMapping(value = "delete/{id}")
+    public ModelAndView delete(@PathVariable("id") Long id, Model model) {
+        userRepository.deleteUser(id);
+        model.addAttribute("userList", getUserList());
+        model.addAttribute("title", "删除用户");
+        return new ModelAndView("users/list", "userModel", model);
+    }
+
+    @GetMapping(value = "modify/{id}")
+    public ModelAndView modifyForm(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("title", "修改用户");
+        return new ModelAndView("users/form", "userModel", model);
+    }
+}
